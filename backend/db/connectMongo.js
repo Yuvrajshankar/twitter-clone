b48bcfa534633c8e76b2b7cf30ea3feeb2eccc8d
Yuvrajshankar <yuvrajpandar2004@gmail.com>
2024-11-08 20:01:56 +0530

    connection

--- backend/db/connectMongo.js
+++ backend/db/connectMongo.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectMongoDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.Mongo_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error connection to mongoDB: ${error.message}`);
